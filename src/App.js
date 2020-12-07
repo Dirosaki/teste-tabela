bd3a39130bb894017a4042279f34808088886dda
Dirosaki <diegosgomes27@gmail.com>
2020-12-07 03:59:02 -0300

    Adição da funcionalidade de requisão de dados e de exclusão de dados

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css';
 import Line from './components/line/';
 import firebase from './config/firebase';
 
 function App() {
-  
-  const [tabelas, setTabelas] = useState();
+
+  const [tabelas, setTabelas] = useState([]);
 
   let listaTabelas = [];
 
-  firebase.firestore().collection('eventos').get().then(async (resultado) => {
-    await resultado.docs.forEach(doc => {
-        
-            listaTabelas.push({
-                id: doc.id,
-                ...doc.data()
-            });
-        
+  useEffect(() => {
+    firebase.firestore().collection('tabela').get().then(async (resultado) => {
+      await resultado.docs.forEach(doc => {
+        listaTabelas.push({
+          id: doc.id,
+          ...doc.data()
+        });
+      });
+      setTabelas(listaTabelas);
     });
-    setTabelas(tabelas);
-});
+  });
 
   return (
-    <div className="corpo d-flex flex-column align-items-center">
+    <div className="corpo d-flex flex-column align-items-center pb-5">
       <img className='m-3' src="https://softwrap.com.br/img/logo/logo-softwrap-branco.png" alt="Logo Softwrap" />
       <div className='tabela-contain m-auto rounded position-relative'>
         <table className="table table-hover tabela">
@@ -39,11 +39,8 @@ function App() {
           </thead>
           <tbody>
             <Line />
-            <Line />
-            <Line />
-            <Line />
-            <Line />
-            <Line />
+            {tabelas.map(item => <Line key={item.id} id={item.id} name={item.nome} age={item.idade} estdCivil={item.estadoCivil} cpfNumber={item.cpf} city={item.cidade} estadoName={item.estado} />)
+            }
           </tbody>
         </table>
       </div>
--- src/components/line/index.js
+++ src/components/line/index.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
-import InputMask from 'react-input-mask';
 import firebase from '../../config/firebase';
 import './line.css';
 
-function Line() {
+function Line({ id, name, age, estdCivil, cpfNumber, city, estadoName }) {
+    // console.log(id)
     const [nome, setNome] = useState();
     const [idade, setIdade] = useState();
     const [estadoCivil, setEstadoCivil] = useState();
@@ -27,61 +27,124 @@ function Line() {
             alert(erro);
         });
     }
+    function excluir() {
+        firebase.firestore().collection('tabela').doc(id).delete().then(() => {
+            alert(id);
+        });
+    }
 
     return (
         <tr>
-            
-            <td><input onChange={(e) => setNome(e.target.value)} className='form-control' type='text' placeholder='Diego' /></td>
-            <td><input onChange={(e) => setIdade(e.target.value)} className='form-control' type='number' placeholder='24' /></td>
-            <td>
-                <select onChange={(e) => setEstadoCivil(e.target.value)} className="form-control">
-                    <option disabled selected>Selecione</option>
-                    <option>Solteiro</option>
-                    <option>Casado</option>
-                    <option>Separado</option>
-                    <option>Divorciado</option>
-                    <option>Viúvo</option>
-                </select>
-            </td>
-            <td><input onChange={(e) => setCpf(e.target.value)}className='form-control' type='number' placeholder='999.999.999-99'/></td>
-            <td><input onChange={(e) => setCidade(e.target.value)} className='form-control' type='text' placeholder='São Paulo' /></td>
-            <td>
-                <select onChange={(e) => setEstado(e.target.value)} className='form-control' id="estado" name="estado">
-                    <option disabled selected>UF</option>
-                    <option>SP</option>
-                    <option>AC</option>
-                    <option>AL</option>
-                    <option>AP</option>
-                    <option>AM</option>
-                    <option>BA</option>
-                    <option>CE</option>
-                    <option>DF</option>
-                    <option>ES</option>
-                    <option>GO</option>
-                    <option>MA</option>
-                    <option>MT</option>
-                    <option>MS</option>
-                    <option>MG</option>
-                    <option>PA</option>
-                    <option>PB</option>
-                    <option>PR</option>
-                    <option>PR</option>
-                    <option>PI</option>
-                    <option>RJ</option>
-                    <option>RN</option>
-                    <option>RS</option>
-                    <option>RO</option>
-                    <option>RR</option>
-                    <option>SC</option>
-                    <option>SP</option>
-                    <option>SE</option>
-                    <option>TO</option>
-                </select>
-            </td>
-            <th>
-                <button onClick={save} type='button' className='btn btn-dark mr-1'>Salvar</button>
-                <button disabled type='button' className='btn btn-danger ml-1'>Excluir</button>
-            </th>
+            {
+                id != undefined
+                    ?
+                    <>
+                        <td><input onChange={(e) => setNome(e.target.value)} className='form-control' type='text' disabled placeholder='Nome' value={name} /></td>
+                        <td><input onChange={(e) => setIdade(e.target.value)} className='form-control' type='number' disabled placeholder='24' value={age} /></td>
+                        <td>
+                            <select onChange={(e) => setEstadoCivil(e.target.value)} disabled className="form-control" value={estdCivil} >
+                                <option disabled selected hidden >Selecione</option>
+                                <option>Solteiro</option>
+                                <option>Casado</option>
+                                <option>Separado</option>
+                                <option>Divorciado</option>
+                                <option>Viúvo</option>
+                            </select>
+                        </td>
+                        <td><input onChange={(e) => setCpf(e.target.value)} className='form-control' type='number' disabled placeholder='999.999.999-99' value={cpfNumber} /></td>
+                        <td><input onChange={(e) => setCidade(e.target.value)} disabled className='form-control' type='text' placeholder='São Paulo' value={city} /></td>
+                        <td>
+                            <select onChange={(e) => setEstado(e.target.value)} disabled className='form-control' value={estadoName}>
+                                <option disabled selected hidden>UF</option>
+                                <option>SP</option>
+                                <option>AC</option>
+                                <option>AL</option>
+                                <option>AP</option>
+                                <option>AM</option>
+                                <option>BA</option>
+                                <option>CE</option>
+                                <option>DF</option>
+                                <option>ES</option>
+                                <option>GO</option>
+                                <option>MA</option>
+                                <option>MT</option>
+                                <option>MS</option>
+                                <option>MG</option>
+                                <option>PA</option>
+                                <option>PB</option>
+                                <option>PR</option>
+                                <option>PE</option>
+                                <option>PI</option>
+                                <option>RJ</option>
+                                <option>RN</option>
+                                <option>RS</option>
+                                <option>RO</option>
+                                <option>RR</option>
+                                <option>SC</option>
+                                <option>SP</option>
+                                <option>SE</option>
+                                <option>TO</option>
+                            </select>
+                        </td>
+                        <th>
+                            <button onClick={save} type='button' className='btn btn-warning mr-1 '>Editar</button>
+                            <button onClick={excluir} type='button' className='btn btn-danger ml-1 text-black'>Excluir</button>
+                        </th>
+                    </>
+                    :
+                    <>
+                        <td><input onChange={(e) => setNome(e.target.value)} className='form-control border-success' type='text' placeholder='Nome' value={name} /></td>
+                        <td><input onChange={(e) => setIdade(e.target.value)} className='form-control border-success' type='number' placeholder='Idade' value={age} /></td>
+                        <td>
+                            <select onChange={(e) => setEstadoCivil(e.target.value)} className="form-control border-success" value={estdCivil} >
+                                <option disabled selected hidden >Selecione</option>
+                                <option>Solteiro</option>
+                                <option>Casado</option>
+                                <option>Separado</option>
+                                <option>Divorciado</option>
+                                <option>Viúvo</option>
+                            </select>
+                        </td>
+                        <td><input onChange={(e) => setCpf(e.target.value)} className='form-control border-success' type='number' placeholder='999.999.999-99' value={cpfNumber} /></td>
+                        <td><input onChange={(e) => setCidade(e.target.value)} className='form-control border-success' type='text' placeholder='São Paulo' value={city} /></td>
+                        <td>
+                            <select onChange={(e) => setEstado(e.target.value)} className='form-control border-success' value={estadoName}>
+                                <option disabled selected hidden>UF</option>
+                                <option>SP</option>
+                                <option>AC</option>
+                                <option>AL</option>
+                                <option>AP</option>
+                                <option>AM</option>
+                                <option>BA</option>
+                                <option>CE</option>
+                                <option>DF</option>
+                                <option>ES</option>
+                                <option>GO</option>
+                                <option>MA</option>
+                                <option>MT</option>
+                                <option>MS</option>
+                                <option>MG</option>
+                                <option>PA</option>
+                                <option>PB</option>
+                                <option>PR</option>
+                                <option>PE</option>
+                                <option>PI</option>
+                                <option>RJ</option>
+                                <option>RN</option>
+                                <option>RS</option>
+                                <option>RO</option>
+                                <option>RR</option>
+                                <option>SC</option>
+                                <option>SP</option>
+                                <option>SE</option>
+                                <option>TO</option>
+                            </select>
+                        </td>
+                        <th>
+                            <button onClick={save} type='button' className='btn btn-success btn-block mr-1'>Salvar</button>
+                        </th>
+                    </>
+            }
         </tr>
     );
 }
